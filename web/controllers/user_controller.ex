e66bd1ed1de9f7304d0584ccd51b9af81c2d7146
Ken Cheng <kenny851017@gmail.com>
2016-10-05 23:46:40 -0400

    just added the username Ken to user_controller.ex, not sure if that's the way to do it, just playing around!

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,6 +2,6 @@ defmodule GaleServer.UserController do
   use GaleServer.Web, :controller
 
   def get_users(conn, _params) do
-    render conn, "users.json", users: [%{username: "chris"}, %{username: "adam"}]
+    render conn, "users.json", users: [%{username: "chris"}, %{username: "adam"}, %{username: "ken"}]
   end
 end
